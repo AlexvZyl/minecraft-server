205f82ed3faa8133cf4503bceaff3bdb778fdd53
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-02 23:17:51 +0200

    Change yacl version

--- modpack/generate.sh
+++ modpack/generate.sh
@@ -4,7 +4,6 @@ MOD_FILE="No-Radars-Modrinth.mrpack"
 CF_FILE="No-Radars-CurseForge.zip"
 
 cd minecraft-modpack/modpack/packwiz || cd modpack/packwiz || cd packwiz || exit
-packwiz update --all
 packwiz modrinth export -o $MOD_FILE
 packwiz curseforge export -o $CF_FILE
 
