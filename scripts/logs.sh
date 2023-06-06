c00f0c4c17a64d7ed2ee100503f363d4e7296174
AlexvZyl <alexandervanzyl@protonmail.com>
2023-06-06 11:53:08 +0000

    Scripts: Improve log script

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker logs --tail 1000 minecraft-server_minecraft-server_1 2>&1 | nvim -R - --cmd ":set nowrap" +
+FILE="/tmp/docker-logs.txt"
+docker logs -f --tail 1000 minecraft-server_minecraft-server_1 2>&1 > "$FILE" &
+nvim -R +":set autoread" +":set nowrap" + "$FILE"
